dd57fb85d72a7e675e23dace532a1be0a664024f
Pranav Jha <pranavjha1993@gmail.com>
2024-12-12 21:48:12 +0530

    <add>migrate from uv to poetry</add>

--- ts/setup.py
+++ ts/setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="ts",
     version="0.0.1",
     author="Pranav Jha",
     author_email="pranavjha1993@gmail.com",
     description="A short description of the project.",
-    url="url-to-github-page",
+    url="https://github.com/Jha-Pranav/ts.git",
     packages=find_packages(),
     test_suite="src.tests.test_all.suite",
 )
