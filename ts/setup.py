06b3ead7c1f097d16f4bf6f4a35462a802a05ed2
Pranav Jha <pranavjha1993@gmail.com>
2025-03-13 12:50:37 +0530

    <chores></chores>

--- ts/setup.py
+++ ts/setup.py
@@ -2,11 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="ts",
-    version="0.0.1",
+    version="0.0.2",
     author="Pranav Jha",
     author_email="pranavjha1993@gmail.com",
     description="A short description of the project.",
     url="https://github.com/Jha-Pranav/ts.git",
-    packages=find_packages(),
-    test_suite="src.tests.test_all.suite",
+    packages=["ts"],
 )
